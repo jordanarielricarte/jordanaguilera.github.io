ab0e894f423b743ff6894f27ed6cc2afa1c82277
jordna <aimbox_prox@hotmail.cl>
2018-11-26 07:18:27 -0600

    primer commit

---
+++ javascript/ValidarNom.js
@@ -0,0 +1,18 @@
+function soloLetras(e){
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = "abcdefghijklmnñopqrstuvwxyz";
+    especiales = "8-37-38-46-164";
+
+    tecla_especial = false
+    for(var i in especiales){
+         if(key == especiales[i]){
+             tecla_especial = true;
+             break;
+         }
+     }
+
+     if(letras.indexOf(tecla)==-1 && !tecla_especial){
+         return false;
+     }
+ }
