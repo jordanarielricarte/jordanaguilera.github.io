1f20bc7b8aabd0c75bfee9c2fb85d0ef25105965
jordna <aimbox_prox@hotmail.cl>
2018-11-26 09:52:12 -0600

    Se actualiza service worked

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+( function() {
+
+    if ('serviceWorker' in navigator) {
+
+        navigator.serviceWorker
+
+                 .register('./service-worker.js')
+
+                 .then(function() { 
+
+                     console.log('Service Worker Registered'); 
+
+                });
+
+      }
+
+} )( );
